4070bb279e45f99ff80cdb977b7dd6bbe886ab11
William M <cominatyou@cominatyou.com>
2021-11-29 00:23:26 -0600

    Add in progress changes

---
+++ app/src/main/java/com/cominatyou/silverpoint/DebugPanel.java
@@ -0,0 +1,14 @@
+package com.cominatyou.silverpoint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DebugPanel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_debug_panel);
+    }
+}
---
+++ app/src/main/java/com/cominatyou/silverpoint/EndpointType.java
@@ -0,0 +1,2 @@
+package com.cominatyou.silverpoint;public enum EndpointType {
+}
