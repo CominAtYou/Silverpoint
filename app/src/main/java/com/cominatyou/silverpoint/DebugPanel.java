3bbaffdc6423e432c02f1764f47ebe6f251f9261
William M <cominatyou@cominatyou.com>
2022-01-15 21:55:38 -0600

    Call toUpperCase() with locale

--- app/src/main/java/com/cominatyou/silverpoint/DebugPanel.java
+++ app/src/main/java/com/cominatyou/silverpoint/DebugPanel.java
@@ -71,7 +71,7 @@ public class DebugPanel extends AppCompatActivity {
                     binding.latestIncidentId.setText(latestIncidentID.equals("") ? "null" : latestIncidentID);
                     final String latestIncidentUpdateID = ActiveIncidentUtil.getLatestUpdateId(getApplicationContext());
                     binding.latestIncidentUpdateId.setText(latestIncidentUpdateID.equals("") ? "null" : latestIncidentUpdateID);
-                    binding.versionField.setText(String.format(Locale.getDefault(), "%s %s, %d", BuildConfig.VERSION_NAME, BuildConfig.BUILD_TYPE.toUpperCase(), BuildConfig.VERSION_CODE));
+                    binding.versionField.setText(String.format(Locale.getDefault(), "%s %s, %d", BuildConfig.VERSION_NAME, BuildConfig.BUILD_TYPE.toUpperCase(Locale.getDefault()), BuildConfig.VERSION_CODE));
                     final ZonedDateTime buildTime = ZonedDateTime.ofInstant(Instant.ofEpochMilli(Long.parseLong(BuildConfig.BUILD_TIME)), TimeZone.getTimeZone("America/Chicago").toZoneId());
                     binding.buildTimestamp.setText(buildTime.format(DateTimeFormatter.ofPattern("MMMM d, yyyy h:KK a z")));
                 });
